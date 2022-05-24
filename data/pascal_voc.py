75bebdc44374b3a166d60c9f42481313b8f3dfe6
Monish-Natarajan <85455908+Monish-Natarajan@users.noreply.github.com>
2022-05-24 14:03:24 +0530

    custom dataset handler

--- data/pascal_voc.py
+++ data/pascal_voc.py
@@ -3,34 +3,25 @@ import collections
 import numpy as np
 import xml.etree.ElementTree as ET
 from PIL import Image
-from torch.utils.data import Dataset
+import torch
+from torch.utils.data import Dataset, DataLoader
+from .data import transforms as Trs
 
 class VOC_seg(Dataset):
-    def __init__(self, data_root, data_mode='val', pseudo_label_dir='/kaggle/input/agv-bana-voc/kaggle/working/data/VOCdevkit/VOC2012/Generation', transforms=None):
+    def __init__(self, data_root, data_mode='train_weak', transforms=None):
         self.train = False
         if data_mode == "train_weak":
-            txt_name = "train_aug.txt"
-            f_path = os.path.join(data_root, "ImageSets/SegmentationAug", txt_name)
+            txt_name = "trainaug.txt"
+            f_path = os.path.join(data_root, "ImageSets", txt_name)
             self.train = True
-        if data_mode == "val":
-            txt_name = "val.txt"
-            f_path = os.path.join(data_root, "ImageSets/Segmentation", txt_name)
-        if data_mode == "test":
-            txt_name = "test.txt"
-            f_path = os.path.join(data_root, "ImageSets/Segmentation", txt_name)
         
         self.filenames = [x.split('\n')[0] for x in open(f_path)]
         self.transforms = transforms
-        
-        self.annot_folders = ["SegmentationClassAug"]
-        if data_mode == "train_weak":
-            self.annot_folders += pseudo_label_dir
-        if data_mode == "test":
-            self.annot_folders = None
-        
-        self.img_path  = os.path.join(data_root, "JPEGImages", "{}.jpg")
-        if self.annot_folders is not None:
-            self.mask_paths = [os.path.join(data_root, folder, "{}.png") for folder in self.annot_folders]
+
+        self.img_path  = os.path.join(data_root, "Images", "{}.jpg")
+        self.pred_masks_path = os.path.join(data_root,'PredictedMasks','{}.png')
+        self.gt_masks_path = os.path.join(data_root,'SegmentationAugmentedMasks','{}.png')
+      
         self.len = len(self.filenames)
         print("Number of Files Loaded: ", self.len)
     
@@ -39,13 +30,26 @@ class VOC_seg(Dataset):
     
     def __getitem__(self, index):
         fn  = self.filenames[index]
-        img = np.array(Image.open(self.img_path.format(fn)), dtype=np.float32) 
-        if self.annot_folders is not None:
-            masks = [np.array(Image.open(mp.format(fn)), dtype=np.int64) for mp in self.mask_paths]
-        else:
-            masks = None
-            
+        img = np.array(Image.open(self.img_path.format(fn)), dtype=np.float32)
+        masks = np.asarray([np.array(Image.open(self.pred_masks_path.format(fn)), dtype=np.float32),
+                 np.array(Image.open(self.gt_masks_path.format(fn)), dtype=np.float32)])
+
         if self.transforms != None:
             img, masks = self.transforms(img, masks)
         
         return img, masks
+
+    if name == '__main__':
+        tr_transforms = Compose([
+            RandomScale(0.5, 1.5),
+            ResizeRandomCrop((321, 321)), 
+            RandomHFlip(0.5), 
+            ColorJitter(0.5,0.5,0.5,0),
+            Normalize_Caffe(),
+            ])
+
+    mydataset = VOC_seg(data_root='/content/data/VOC_toy',data_mode='train_weak',transforms=tr_transforms)
+    tloader = DataLoader(mydataset, batch_size=2, shuffle=False, num_workers=2)
+    for img,mask in tloader:
+        print(img.shape)
+        print(mask[0].shape)  
